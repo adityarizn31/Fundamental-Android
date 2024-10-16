9917e2bbf9acb5ab8c843381deab3964fb572671
[adityarn] <[adityarizkiawann@gmail.com]>
2024-10-16 18:23:24 +0700

    Belajar Flexible Fragment

--- app/src/main/java/com/example/latihanfragment/CategoryFragment.kt
+++ app/src/main/java/com/example/latihanfragment/CategoryFragment.kt
@@ -14,11 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [CategoryFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-
-class HomeFragment : Fragment(), View.OnClickListener {
+class CategoryFragment : Fragment(), View.OnClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,7 +35,7 @@ class HomeFragment : Fragment(), View.OnClickListener {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_category, container, false)
     }
 
     companion object {
@@ -46,12 +45,12 @@ class HomeFragment : Fragment(), View.OnClickListener {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment CategoryFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            CategoryFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
@@ -62,19 +61,13 @@ class HomeFragment : Fragment(), View.OnClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val btnCategory: Button = view.findViewById(R.id.btn_category)
-        btnCategory.setOnClickListener(this)
+        val btnDetailCategory:Button = view.findViewById(R.id.btn_detail_category)
+        btnDetailCategory.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        if (v?.id == R.id.btn_category) {
-            val categoryFragment = CategoryFragment()
-            val fragmentManager = parentFragmentManager
-            fragmentManager.beginTransaction().apply {
-                replace(R.id.frame_container, categoryFragment, CategoryFragment::class.java.simpleName)
-                addToBackStack(null)
-                commit()
-            }
+        if (v?.id === R.id.btn_detail_category) {
+
         }
     }
 }
