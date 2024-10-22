fa5f1b78e8091d05784ed855b1c384f9d92a84bd
[adityarn] <[adityarizkiawann@gmail.com]>
2024-10-22 11:31:27 +0700

    Modified add Executor and Handler

--- app/src/main/java/com/example/latihanasyncronusbackgroundthread/MainActivity.kt
+++ app/src/main/java/com/example/latihanasyncronusbackgroundthread/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.latihanasyncronusbackgroundthread
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,19 +25,31 @@ class MainActivity : AppCompatActivity() {
         val btnStart = findViewById<Button>(R.id.btn_start)
         val tvStatus = findViewById<TextView>(R.id.tv_status)
 
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
         btnStart.setOnClickListener {
-            try {
-                for (i in 0..10) {
-                    Thread.sleep(500)
-                    val percentage = i * 10
-                    if (percentage == 100) {
-                        tvStatus.setText(R.string.task_completed)
-                    } else {
-                        tvStatus.text = String.format(getString(R.string.compressing), percentage)
+
+            executor.execute {
+
+                try {
+                    for (i in 0..10) {
+                        Thread.sleep(500)
+                        val percentage = i * 10
+
+                        handler.post {
+
+                            if (percentage == 100) {
+                                tvStatus.setText(R.string.task_completed)
+                            } else {
+                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                            }
+
+                        }
                     }
+                } catch (e : InterruptedException) {
+                    e.printStackTrace()
                 }
-            } catch (e : InterruptedException) {
-                e.printStackTrace()
             }
         }
     }
