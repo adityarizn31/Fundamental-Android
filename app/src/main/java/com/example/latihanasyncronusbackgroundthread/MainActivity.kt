44b6579611fdea679ada2ce2db097f92dcad8015
[adityarn] <[adityarizkiawann@gmail.com]>
2024-10-22 11:51:14 +0700

    Menambahkan Coroutine dan mengganti Executor

--- app/src/main/java/com/example/latihanasyncronusbackgroundthread/MainActivity.kt
+++ app/src/main/java/com/example/latihanasyncronusbackgroundthread/MainActivity.kt
@@ -9,6 +9,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
@@ -25,32 +30,23 @@ class MainActivity : AppCompatActivity() {
         val btnStart = findViewById<Button>(R.id.btn_start)
         val tvStatus = findViewById<TextView>(R.id.tv_status)
 
-        val executor = Executors.newSingleThreadExecutor()
-        val handler = Handler(Looper.getMainLooper())
-
         btnStart.setOnClickListener {
 
-            executor.execute {
-
-                try {
-                    for (i in 0..10) {
-                        Thread.sleep(500)
-                        val percentage = i * 10
-
-                        handler.post {
-
-                            if (percentage == 100) {
-                                tvStatus.setText(R.string.task_completed)
-                            } else {
-                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
-                            }
-
+            lifecycleScope.launch(Dispatchers.Default) {
+//                Simulate Coroutine Background Thread
+                for (i in 0 .. 10) {
+                    delay(500)
+                    val percentage = i * 10
+                    withContext(Dispatchers.Main) {
+                        if (percentage == 100) {
+                            tvStatus.setText(R.string.task_completed)
+                        } else {
+                            tvStatus.text = String.format(getString(R.string.compressing), percentage)
                         }
                     }
-                } catch (e : InterruptedException) {
-                    e.printStackTrace()
                 }
             }
+
         }
     }
 }
