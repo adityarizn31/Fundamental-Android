b0dd68c7cb07741f1313d852cfbff294c0504fef
[adityarn] <[adityarizkiawann@gmail.com]>
2024-10-31 11:14:43 +0700

    Belajar ViewModel, Menambahkan MainBinding dan ViewModel

---
+++ app/src/main/java/com/example/latihanviewmodel/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.latihanviewmodel
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.latihanviewmodel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding : ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        displayResult()
+
+        activityMainBinding.btnCalculate.setOnClickListener {
+            val width = activityMainBinding.edtWidth.text.toString()
+            val height = activityMainBinding.edtHeight.text.toString()
+            val length = activityMainBinding.edtLength.text.toString()
+
+            when {
+                width.isEmpty() -> {
+                    activityMainBinding.edtWidth.error = "Masih Kosong"
+                }
+                height.isEmpty() -> {
+                    activityMainBinding.edtHeight.error = "Masih Kosong"
+                }
+                length.isEmpty() -> {
+                    activityMainBinding.edtLength.error = "Masih Kosong"
+                }
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+        }
+    }
+
+    private fun displayResult() {
+        activityMainBinding.tvResult.text = viewModel.result.toString()
+    }
+}
